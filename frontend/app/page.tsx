3718470f323829f6b1aaa1ef303da52dcd0a5aa5
ahmad <ahmad.qeis122@gmail.com>
2026-01-14 18:08:49 +0700

    a

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -49,8 +49,8 @@ export default function HomePage() {
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
               Platform digital untuk pengelolaan data dan monitoring kesejahteraan lansia
             </p>
-            <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl md:text-6xl">
-              {user ? `Selamat datang, ${user?.username}` : 'Selamat datang di Sistem Sepuh Cipamokolan'}
+            <h1 className="text-2xl font-bold text-gray-700 sm:text-1xl md:text-1xl">
+              {user ? `Halo, ${user?.username}!` : 'Halo, Guest!'}
             </h1>
             <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
               <div className="rounded-md shadow">
